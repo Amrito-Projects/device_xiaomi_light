ab9ec865990c774ec3137d6aa73696af38bdbfef
Amrito1 <bhabanidas431@gmail.com>
2024-01-05 23:08:48 +0530

    light: Configure Thermal stack

--- device.mk
+++ device.mk
@@ -264,6 +264,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@2.0.vendor
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/light/light-vendor.mk
