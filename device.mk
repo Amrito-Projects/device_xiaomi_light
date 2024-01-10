75ade06cb3f77992436e79ecafa8067d4fe40706
Amrito1 <bhabanidas431@gmail.com>
2024-01-10 13:11:17 +0530

    light: Reconfigure IMS/RADIO stack

--- device.mk
+++ device.mk
@@ -260,6 +260,12 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+
+# Radio
+PRODUCT_PACKAGES += \
+    android.hardware.radio.config@1.3.vendor \
+    android.hardware.radio@1.6.vendor
+
 # IMS
 PRODUCT_BOOT_JARS += \
     mediatek-common \
