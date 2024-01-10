94d20990fa598c3ea3ea4ce473fb876c0f8b4808
Sushrut1101 <guptasushrut@gmail.com>
2024-01-10 17:35:29 +0530

    light: Configure Hotword Enrollment stack
    Change-Id: I386f3172acb7755a68c69a241a065b163183ec59
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -307,6 +307,11 @@ PRODUCT_PACKAGES += \
     TetheringConfigOverlayLight \
     WifiOverlayLight
 
+# Hotword Enrollment
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.android.hotwordenrollment.common.util.xml \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-hotword.xml
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
